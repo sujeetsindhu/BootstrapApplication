0a697de9c832a04d0406305795d2eade2c3f775e
Sujeet Sindhu <sujeetsindhu@gmail.com>
2018-10-08 12:35:58 +0530

    New bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { FigureComponent } from './figure/figure.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FigureComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '' , component: HomeComponent},
+      {path: 'figure', component: FigureComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
